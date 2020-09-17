59563156759295742efb3bae4e4a269881fe6073
Chris L <c.aventine@gmail.com>
2020-09-17 16:51:58 -0500

    Finish

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Turtle
+from .models import Turtle, Feeding
 
 # Register your models here
 admin.site.register(Turtle)
+admin.site.register(Feeding)
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-from .models import Turtle
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Turtle, Toy
+from .forms import FeedingForm
+
 
 # Add the following import
 from django.http import HttpResponse
@@ -17,4 +21,57 @@ def turtles_index(request):
 
 def turtles_detail(request, turtle_id):
   turtle = Turtle.objects.get(id=turtle_id)
-  return render(request, 'turtles/detail.html', { 'turtle': turtle })
+  toys_turtle_doesnt_have = Toy.objects.exclude(id__in = turtle.toys.all().values_list('id'))
+  feeding_form = FeedingForm()
+  return render(request, 'turtles/detail.html', {
+    'turtle': turtle, 'feeding_form': feeding_form,
+    'toys': toys_turtle_doesnt_have
+  })
+
+def add_feeding(request, turtle_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.turtle_id = turtle_id
+    new_feeding.save()
+  return redirect('detail', turtle_id=turtle_id)
+
+def assoc_toy(request, turtle_id, toy_id):
+  # Note that you can pass a toy's id instead of the whole object
+  Turtle.objects.get(id=turtle_id).toys.add(toy_id)
+  return redirect('detail', turtle_id=turtle_id)
+
+def unassoc_toy(request, turtle_id, toy_id):
+  # Note that you can pass a toy's id instead of the whole object
+  Turtle.objects.get(id=turtle_id).toys.remove(toy_id)
+  return redirect('detail', turtle_id=turtle_id)
+
+class TurtleCreate(CreateView):
+  model = Turtle
+  fields = '__all__'
+
+class TurtleUpdate(UpdateView):
+  model = Turtle
+  fields = ['species', 'description', 'age']
+
+class TurtleDelete(DeleteView):
+  model = Turtle
+  success_url = '/turtles/'
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
