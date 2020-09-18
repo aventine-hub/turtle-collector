4f67092cdd0be5290317c339f72fa3962e759726
Chris L <c.aventine@gmail.com>
2020-09-18 09:27:41 -0500

    Update

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Turtle, Feeding
+from .models import Turtle, Feeding, Toy
 
 # Register your models here
 admin.site.register(Turtle)
 admin.site.register(Feeding)
+admin.site.register(Toy)
